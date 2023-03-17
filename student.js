8ad34834f5ec253985946721db4b5161c1e745ad
shuvo9620 <smahfuzur0@gmail.com>
2023-03-17 15:30:55 +0600

    solution

---
+++ student.js
@@ -0,0 +1,5 @@
+// Question2: how we can make our class room students.. name alphabetically?
+
+const studentsName = ["abul", "kashem", "banul", "zahim", "cmarim", "thanto"];
+
+console.log("students names are: " + studentsName.sort());
