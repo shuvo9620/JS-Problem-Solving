79a25ab1fbc27c5d28dc17260cd963f27f337e53
shuvo9620 <smahfuzur0@gmail.com>
2023-03-17 15:12:14 +0600

    print done

--- random.js
+++ random.js
@@ -1,3 +1,6 @@
 // question: Write a code to print a random number between 1 to 6 like a ludo dice.
+function getRandomNumber(min, max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
-
+console.log(getRandomNumber(1, 6));
